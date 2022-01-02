4ce831d0552efd7dd0da2a95be2f20054b75f313
Gabriela <gbraga@uwo.ca>
2022-01-02 17:17:26 -0500

    new Message Service allows components to log messages which are displayed in the new Messages Component

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  /*  the messageService property must be public so that Angular can bind the template to it */
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit(): void {
+  }
+
+}
